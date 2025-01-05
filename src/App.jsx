9d6a8ab6a10fca4e1ebf1e49ed0abf4a8f5914bf
Tiran Wijerathne <tiranwijeratne@gmail.com>
2025-01-05 11:34:52 +0530

    app.jsx update
    app.js updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import "./App.css";
 
 
 //Website
-// import Navbar from './Components/Navbar';
-// import Home from './Screens/Home';
-// import Footer from "./Components/Footer";
-// import Career from "./Screens/Career";
-// import JobDetails from "./Screens/JobDetails"
-// import ContactPage from "./Screens/ContactPage";
-// import ServicesPage from "./Screens/ServicesPage";
-// import AboutUs from "./AboutUsPage/AboutUs";
+import Navbar from './Components/Navbar';
+import Home from './Screens/Home';
+import Footer from "./Components/Footer";
+import Career from "./Screens/Career";
+import JobDetails from "./Screens/JobDetails"
+import ContactPage from "./Screens/ContactPage";
+import ServicesPage from "./Screens/ServicesPage";
+import AboutUs from "./AboutUsPage/AboutUs";
 
 
 //HR
@@ -39,36 +39,7 @@ function App() {
       </div>
       {/* <MainHR /> */}
     </Router>
-
-
-function App() {
-  return (
-    <div>
-
-      <Router>
-        <div className="App">
-
-{/* website pages */}
-          {/* <Navbar/>
-          <Routes>
-            <Route path="/home" element={<Home/>} />
-            <Route path="/aboutus" element={<AboutUs/>} />
-            
-            <Route path="/" element={<Home/>} />
-            <Route path="/career" element={<Career/>} />
-            <Route path="/contact" element={<ContactPage/>} />
-            <Route path="/services" element={<ServicesPage/>} />
-            <Route path="/career/:id" element={<JobDetails/>} />
-          </Routes>
-
-          <Footer/> */}
-
-{/* HR Pages */}
-          <MainHR />
-
-        </div>
-      </Router> 
-    </div>
+    
   );
 }
 
