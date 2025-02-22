33cf7fc536b8e2f0f708f89e3a01d6d81ca26a1e
Tiran Wijerathne <tiranwijeratne@gmail.com>
2025-02-22 23:22:48 +0530

    Update Career.jsx

--- src/Screens/Career.jsx
+++ src/Screens/Career.jsx
@@ -47,7 +47,7 @@
         const [filteredJobs, setFilteredJobs] = useState([]);
       
         useEffect(() => {
-          axios.get('http://localhost:8070/vacancies/Vview')
+          axios.get('https://hr-management-backend-pde8.vercel.app/vacancies/Vview')
             .then((response) => {
               setJobs(response.data);
               setFilteredJobs(response.data);
