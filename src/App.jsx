c5d08a3fa0ce42647f871d5b8502e0d7ab5b0df6
Tiran-Sarith <tiranwijeratne@gmail.com>
2025-05-06 23:36:14 +0530

    HR page and client page
    App js issue solved. app.jsx and MainHR solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-
-//App.jsx
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 
-
 //Website
-
 import Navbar from './Components/Navbar';
 import Home from './Screens/Home';
 import Footer from "./Components/Footer";
@@ -20,52 +16,85 @@ import MainHR from "./HRPages/MainHR";
 import JobApplication from "./JobApplication/JobApplication";
 import HRLogin from "./Screens/HRLogin";
 
-
-
+// Layout component that conditionally renders Navbar and Footer
+const Layout = ({ children }) => {
+  const location = useLocation();
+  
+  // Check if the current path is HR-related
+  const isHRPage = location.pathname.includes('/hrHome') || 
+                   location.pathname.includes('/hrlogin');
+  
+  return (
+    <>
+      {!isHRPage && <Navbar />}
+      {children}
+      {!isHRPage && <Footer />}
+    </>
+  );
+};
 
 function App() {
   return (
-
     <div>
-
       <Router>
         <div className="App">
-
-{/* website pages */}
-
-          {/* <Navbar/>
           <Routes>
-
-            <Route path="/apply" element={<JobApplication/>}/>
-
-            <Route path="/home" element={<Home/>} />
-            <Route path="/" element={<Home/>} />
-            <Route path="/hrlogin" element={<HRLogin/>} />
-            <Route path="/career" element={<Career/>} />
-            <Route path="/contact" element={<ContactPage/>} />
-            <Route path="/services" element={<ServicesPage/>} />
-            <Route path="/aboutus" element={<About/>} />
-            <Route path="/career/:id" element={<JobDetails/>} />
-            <Route path="/career/:id/apply" element={<JobApplication/>} />
-            <Route path="/hrlogin" element={<HRLogin/>} /> 
-            <Route path="/hrHome" element={<MainHR/>} /> 
-
+            <Route path="/" element={
+              <Layout>
+                <Home />
+              </Layout>
+            } />
+            <Route path="/home" element={
+              <Layout>
+                <Home />
+              </Layout>
+            } />
+            <Route path="/career" element={
+              <Layout>
+                <Career />
+              </Layout>
+            } />
+            <Route path="/contact" element={
+              <Layout>
+                <ContactPage />
+              </Layout>
+            } />
+            <Route path="/services" element={
+              <Layout>
+                <ServicesPage />
+              </Layout>
+            } />
+            <Route path="/aboutus" element={
+              <Layout>
+                <About />
+              </Layout>
+            } />
+            <Route path="/career/:id" element={
+              <Layout>
+                <JobDetails />
+              </Layout>
+            } />
+            <Route path="/career/:id/apply" element={
+              <Layout>
+                <JobApplication />
+              </Layout>
+            } />
+            <Route path="/apply" element={
+              <Layout>
+                <JobApplication />
+              </Layout>
+            } />
+            
+            {/* HR pages without navbar and footer */}
+            <Route path="/hrlogin" element={<HRLogin />} />
+            <Route path="/hrHome" element={<MainHR />} />
           </Routes>
-          <Footer/> */}
-
-{/* HR Pages */}
-
-          <MainHR/>
-
-{/*           <MainHR/> */}
-
-
-
-
         </div>
-      </Router> 
-    </div>
+        <MainHR />
+      </Router>
 
+   
+    </div>
   );
 }
 
